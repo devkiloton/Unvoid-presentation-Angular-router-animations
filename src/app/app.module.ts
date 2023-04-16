732ecb7ce7af985eb03ff0289dc2d9938522c20b
devkiloton <dev.kiloton@gmail.com>
2023-04-16 20:21:49 -0300

    Implementing the routes in app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { OneComponent } from './pages/one/one.component'
-import { TwoComponent } from './pages/two/two.component'
-import { ThreeComponent } from './pages/three/three.component'
 
 @NgModule({
   declarations: [AppComponent],
