610175829e65f4895b57af5d6dc9abcdba473768
devkiloton <dev.kiloton@gmail.com>
2023-04-16 20:12:40 -0300

    Defining Lazy loaded routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'one',
+    loadComponent: () =>
+      import('./pages/one/one.component').then(mod => mod.OneComponent),
+  },
+  {
+    path: 'two',
+    loadComponent: () =>
+      import('./pages/two/two.component').then(mod => mod.TwoComponent),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/three/three.component').then(mod => mod.ThreeComponent),
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
