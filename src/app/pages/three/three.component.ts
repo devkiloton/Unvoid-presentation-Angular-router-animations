ac832adf71171f160f0a5efc6d2d83408140741b
devkiloton <dev.kiloton@gmail.com>
2023-04-16 20:11:01 -0300

    Creating the 3 pages that'll be used in the exampl

---
+++ src/app/pages/three/three.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-three',
+  templateUrl: './three.component.html',
+  styleUrls: ['./three.component.scss'],
+  standalone: true,
+})
+export class ThreeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/pages/two/two.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-two',
+  templateUrl: './two.component.html',
+  styleUrls: ['./two.component.scss'],
+  standalone: true,
+})
+export class TwoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
