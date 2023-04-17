8c21a7ede4ceea6897ee354ef62c9441efff3ca4
devkiloton <dev.kiloton@gmail.com>
2023-04-17 07:23:04 -0300

    Implementing the animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { fader } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [fader]
 })
 export class AppComponent {
   title = 'Unvoid-presentation-Angular-router-animations'
+
+  prepareRoute(outlet: RouterOutlet){
+    return outlet && outlet.activatedRoute && outlet.activatedRouteData['animation'];
+  }
 }
